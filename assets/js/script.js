aaafc49c9a2c51ae7db23be9007a25856ee163bd
msnaye <99111936+msnaye@users.noreply.github.com>
2022-04-27 01:00:15 -0500

    gave keys a let variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /***************************
  * Trip Planner, script.js
  ***************************/
- keyRing = {
+   let keyRing = {
    position_stack: "d5f115b51ab45c474ebd586717ea58a8",
    open_weather_map: "5056fb3f5552cba986f4ea65f8eec72e",
    ticketmaster: "d6Hq0N8GnW5O3VKdZ1azVxmghFvXITUv",
