0410651803af2cbb9f080083c94e74fe82ef718e
msnaye <99111936+msnaye@users.noreply.github.com>
2022-04-27 00:52:25 -0500

    moved keys to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 /***************************
  * Trip Planner, script.js
  ***************************/
-
+ keyRing = {
+   position_stack: "d5f115b51ab45c474ebd586717ea58a8",
+   open_weather_map: "5056fb3f5552cba986f4ea65f8eec72e",
+   ticketmaster: "d6Hq0N8GnW5O3VKdZ1azVxmghFvXITUv",
+ };
 // Body elements
 var dayEventsEl = document.querySelector("article.day-events");
 var navFormEl = document.getElementById("nav-form");
